8abed47dde17d68a5326c969c25957f15e0e034a
Dallin Marshall <BlueJarvis23@gmail.com>
2015-11-18 13:30:58 -0700

    Final -- Turn in submission -- No longer equ. to online source
    Changes to be committed:
      deleted:    cachelab_handout/a01073629-handin.tar
      renamed:    cachelab_handout/dallin-handin.tar -> cachelab_handout/a01073629_a01107224-handin.tar
      modified:   cachelab_handout/trace.tmp
      modified:   cachelab_handout/trans.c

--- cachelab_handout/trans.c
+++ cachelab_handout/trans.c
@@ -27,7 +27,7 @@ void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
     int i, j, k, l;
     int tmp=0;
-    int tmp1, tmp2, tmp3, tmp4, tmp5, tmp6, tmp7;
+    int tmp1, tmp2, tmp3;//, tmp4, tmp5, tmp6, tmp7;
     if( M == 32 && N == 32 ){ // Blocked Matrix Transpose -- 8 because b of cache is 5, 32 bytes = 8 ints -- diagonal optimization
         for (i = 0; i < N; i+=8) {
             for (j = 0; j < M; j+=8) {
@@ -50,18 +50,12 @@ void transpose_submit(int M, int N, int A[N][M], int B[M][N])
                     tmp1 = A[j+k][i+1];
                     tmp2 = A[j+k][i+2];
                     tmp3 = A[j+k][i+3];
-                    if(!k){
-                        tmp4 = A[j+k][i+4];
-                        tmp5 = A[j+k][i+5];
-                        tmp6 = A[j+k][i+6];
-                        tmp7 = A[j+k][i+7];
-                    }
                     B[i][j+k] = tmp;
                     B[i][j+k+(1*M)] = tmp1;
                     B[i][j+k+(2*M)] = tmp2;
                     B[i][j+k+(3*M)] = tmp3;
                 }
-                for(k=7; k>0; --k){
+                for(k=7; k>=0; --k){
                     tmp = A[j+k][i+4];
                     tmp1 = A[j+k][i+4+1];
                     tmp2 = A[j+k][i+4+2];
@@ -71,10 +65,6 @@ void transpose_submit(int M, int N, int A[N][M], int B[M][N])
                     B[i+4][j+k+(2*M)] = tmp2;
                     B[i+4][j+k+(3*M)] = tmp3;
                 }
-                B[i+4][j] = tmp4;
-                B[i+4][j+(1*M)] = tmp5;
-                B[i+4][j+(2*M)] = tmp6;
-                B[i+4][j+(3*M)] = tmp7;
             }
         }   
     }
@@ -116,7 +106,7 @@ void trans_32(int M, int N, int A[N][M], int B[M][N]){
 char trans_64_desc[] = "Trans for 64 x 64";
 void trans_64(int M, int N, int A[N][M], int B[M][N]){
     int i, j, k;
-    int tmp, tmp1, tmp2, tmp3, tmp4, tmp5, tmp6, tmp7;
+    int tmp, tmp1, tmp2, tmp3;
     for (i = 0; i < N; i+=8) {
         for (j = 0; j < M; j+=8) {
             for(k = 0; k < 8; ++k){
@@ -124,18 +114,12 @@ void trans_64(int M, int N, int A[N][M], int B[M][N]){
                 tmp1 = A[j+k][i+1];
                 tmp2 = A[j+k][i+2];
                 tmp3 = A[j+k][i+3];
-                if(!k){
-                    tmp4 = A[j+k][i+4];
-                    tmp5 = A[j+k][i+5];
-                    tmp6 = A[j+k][i+6];
-                    tmp7 = A[j+k][i+7];
-                }
                 B[i][j+k] = tmp;
                 B[i][j+k+(1*M)] = tmp1;
                 B[i][j+k+(2*M)] = tmp2;
                 B[i][j+k+(3*M)] = tmp3;
             }
-            for(k=7; k>0; --k){
+            for(k=7; k>=0; --k){
                 tmp = A[j+k][i+4];
                 tmp1 = A[j+k][i+4+1];
                 tmp2 = A[j+k][i+4+2];
@@ -145,10 +129,35 @@ void trans_64(int M, int N, int A[N][M], int B[M][N]){
                 B[i+4][j+k+(2*M)] = tmp2;
                 B[i+4][j+k+(3*M)] = tmp3;
             }
-            B[i+4][j] = tmp4;
-            B[i+4][j+(1*M)] = tmp5;
-            B[i+4][j+(2*M)] = tmp6;
-            B[i+4][j+(3*M)] = tmp7;
+        }
+    }
+}
+
+
+char trans_64_desc1[] = "Trans for 64 x 64";
+void trans_641(int M, int N, int A[N][M], int B[M][N]){
+    int i, j, k;
+    int tmp, tmp1, tmp2, tmp3, tmp4, tmp5, tmp6, tmp7;
+    for (i = 0; i < N; i+=8) {
+        for (j = 0; j < M; j+=8) {
+            for(k = 0; k < 8; ++k){
+                tmp = A[j+k][i];
+                tmp1 = A[j+k][i+1];
+                tmp2 = A[j+k][i+2];
+                tmp3 = A[j+k][i+3];
+                tmp4 = A[j+k][i+4];
+                tmp5 = A[j+k][i+5];
+                tmp6 = A[j+k][i+6];
+                tmp7 = A[j+k][i+7];
+                B[i][j+k] = tmp;
+                B[i][j+k+(1*M)] = tmp1;
+                B[i][j+k+(2*M)] = tmp2;
+                B[i][j+k+(3*M)] = tmp3;
+                B[i][j+k+(4*M)] = tmp4;
+                B[i][j+k+(5*M)] = tmp5;
+                B[i][j+k+(6*M)] = tmp6;
+                B[i][j+k+(7*M)] = tmp7;
+            }
         }
     }   
 }
@@ -198,6 +207,7 @@ void registerFunctions()
     /* Register any additional transpose functions */
     registerTransFunction(trans_32, trans_32_desc); 
     registerTransFunction(trans_64, trans_64_desc); 
+    registerTransFunction(trans_641, trans_64_desc1); 
     registerTransFunction(trans_61x67, trans_61x67_desc); 
 
     registerTransFunction(trans, trans_desc); 
